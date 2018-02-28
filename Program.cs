981b81ca7e6f76c73beba043253a011ab2774ed3
Doug Paice <doug.paice@baralong.org>
2018-02-28 13:06:25 +0800

    init console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace docker_playpen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
